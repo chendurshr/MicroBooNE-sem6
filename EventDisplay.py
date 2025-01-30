cf2d85f273a71997de91eecd1250eb68fc574e5b
chendurshr <142641548+chendurshr@users.noreply.github.com>
2025-01-30 14:28:10 +0000

    Initial commit: Added project files except true_data

---
+++ EventDisplay.py
@@ -0,0 +1,132 @@
+import h5py
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
+import matplotlib.font_manager as fm 
+import numpy as np
+import matplotlib.colors as colors
+
+
+
+##########################################################
+#
+#      Script to visualize larcv images in hdf5 format
+#               Author: Luis Mora Lepin
+#          luis.moralepin@postgrad.manchester.ac.uk
+#
+###########################################################
+
+
+'''
+Things to do: 
+* Add text with run,subrun,event 
+* Where the images will be stored? 
+'''
+
+# Function to get the entry of an event given its labels (run,subrun and event)
+def SearchID(run,subrun,event,id_list):
+    id = 0 
+    # First entry in hdf5 metadata is event number and 3rd entry is run number  
+    for element in id_list:
+        if(element[1]==event and element[2]==subrun and element[3]==run):
+            id = element[0]
+        else:
+            pass
+    # fail case
+    if (id==0):
+        print("No such event found! Please check that such an event exists.")
+        print("Displaying event: {}, subrun: {}, run: {}".format(id_list[0][1], id_list[0][2], id_list[0][3]) )
+    return id 
+
+def PlaneLabel(plane):
+    plane_label = 'none'
+    if(plane == 0):
+        plane_label='U'
+    elif(plane==1):
+        plane_label='V'
+    elif(plane==2):
+        plane_label='W'
+    else:
+        print("Error, plane number out of range")
+    return plane_label 
+
+def DatasetSelector(dataset):
+    file='none'
+    if(dataset == 'mc'):
+        file='bnb_run3_mc_larcv.h5'
+    elif(dataset == 'data'):
+        file='bnb_run3_open_data_larcv.h5'
+    elif(dataset == 'dirt'):
+        file='bnb_run3_dirt_larcv.h5'
+    elif(dataset == 'ext'):
+        file='bnb_run3_ext_larcv.h5'
+    elif(dataset == 'slim'):
+        file='bnb_run3_mc_larcv_slimmed.h5'
+    else:
+        print('Error, wrong dataset input')
+    return file 
+
+
+def EvDisp(run,subrun,event,plane,dataset,debug=False):
+    #base_dir = "/hepgpu4-data2/lmlepin/Ole_files/"
+    # adjust based on where you're saving the data files
+    base_dir = "data/"
+    input_file=base_dir+DatasetSelector(dataset)
+    producer = 'wire'
+    f = h5py.File(input_file,'r')
+    event_id_list = f['eventid']
+    entry = SearchID(run,subrun,event,event_id_list)
+    # restating values of dataset, event, entry, as they can be modified if incorrect value is selected.
+    event = event_id_list[entry][1]
+    subrun = event_id_list[entry][2]
+    run = event_id_list[entry][3]
+    #if(debug):
+    #	entry = 1000
+    #else: 
+    #	entry = SearchID(run,subrun,event,event_id_list)
+    plane_label = PlaneLabel(plane)
+    #print(list(f['image2d'].keys()))
+    #print(list(f['image2d']['metadata']))
+    #print(list(f['image2d']['wire'].keys()))
+    wire_set = f['image2d'][producer]
+    print(wire_set)
+    image=wire_set[list(wire_set.keys())[0]]
+    fontprops=fm.FontProperties(size=10)
+    plot_title = "BNB Run 3, Dataset: " + dataset + ", Plane : " + plane_label + ", Event: " + str(event) + ", Entry: " + str(entry)
+    fig, ax1= plt.subplots(1,1,figsize=(8.5,3.5))
+    plt.title(plot_title)
+    plt.xlabel("Wire number")
+    plt.ylabel("Time tick")
+    ax1.imshow(image[entry,plane,:,:],cmap='jet', origin="lower", norm=colors.PowerNorm(gamma=0.35, vmin=np.min(image[entry,plane,:,:]), vmax=np.max(image[entry,plane,:,:])))
+    #ax1.imshow(image[entry,plane,:,:],cmap='turbo',origin="lower")
+    scalebar1= AnchoredSizeBar(ax1.transData,
+                                100,'30 cm','lower left',
+                                color='white',
+                                frameon=False,
+                                size_vertical=1,
+                                fontproperties=fontprops)
+    ax1.add_artist(scalebar1)
+    plt.show()
+
+    
+def list_entries(dataset, sliced=False, slice_val = 0):
+    base_dir = "data/"
+    input_file=base_dir+DatasetSelector(dataset) 
+    
+    f = h5py.File(input_file,'r')
+    event_id_list = f['eventid']
+    
+    # can remove entries from your printed list, as there are many
+    
+    if sliced == True:    
+        i = 0
+        for element in event_id_list:
+            if i > slice_val:
+                break;
+            print("Event: {}, Subrun: {}, Run: {}".format(element[1], element[2], element[3]))
+            i += 1
+    else:
+        print("Caution: You are printing every event in the file (there are {}).".format(len(event_id_list)))
+        for element in event_id_list:
+            print("Event: {}, Subrun: {}, Run: {}".format(element[1], element[2], element[3]))
+    
+            
---
+++ README.md
@@ -0,0 +1,46 @@
+## 3rd year undergraduate lab focusing on MicroBooNE experiment
+### Developed by O.G. Finnerud, A. Kedziora, & J. Waiton
+
+This folder contains almost everything that is needed to run the lab, including:
+
+#### /data/
+Data no longer included here, available at the link below.
+6 files included:
+- DataSet_LSND.csv: Dataset for LSND used as comparison (42.9KB).
+- DataSet_MiniBooNE.csv: Dataset for MiniBoone used as comparison (17.0KB).
+- data_flattened.pkl: True data from MicroBooNE flattened (24.2MB). (A)
+- MC_EXT_flattened.pkl: MC and EXT data from simulations & MicroBooNE (111.4MB). (A)
+- bnb_run3_mc_larcv_slimmed.h5: Event display file (77.3MB).
+- oscillated_data.pkl: Data file for usage in the closure test. (140MB)
+
+#### NOTE! All files now accessible via this link for University of Manchester students.
+https://livemanchesterac-my.sharepoint.com/:u:/g/personal/john_waiton_postgrad_manchester_ac_uk/ERr7l9mqqGZIveVcVG3UD2wBrCHCYTcJPT7jIpyBOM4zZg?e=Bh2uvB
+
+
+
+#### MainAnalysis_template.ipynb
+Template for student's usage throughout the lab.
+
+#### MicroBooNE_Y3_Labbook.pdf
+PDF including all information needed to complete this lab.
+
+
+#### Neutrino_functions.py
+Useful functions that are used throughout the lab.
+
+
+#### event_display.ipynb & EventDisplay.py
+Allows for visualisation of events.
+
+#### keras_example.ipynb
+An example of a simple NN in action for interest of the reader.
+
+### INSTALLATION AND USAGE ON YEAR 3 LAB MACHINES
+
+##### Ensure you have approximately 10GB of space on your OneDrive for this method
+
+- Download this repository either via git or by pressing the green "Code" button and then "Download ZIP".
+- Download the data required from the above mentioned 'livemanchesterac' link. 
+- Extract the *uboone_lab_student_copy* folder into your local OneDrive folder.
+- Extract the *data.zip* folder into *uboone_lab_student_copy/data/* folder.
+- Begin reading through the rest of the lab script, good luck!
